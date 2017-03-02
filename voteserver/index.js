792887c547e2ae903eeae42b9f73ff17987404bd
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-03-02 14:02:54 +0200

    Added entries.json set connections for actions

--- voteserver/index.js
+++ voteserver/index.js
@@ -1,3 +1,6 @@
 import makeStore from './src/store';
+import startServer from './scr/server';
 
 export const store = makeStore();
+startServer(store);
+//Добавляю команду выполнения запуска сервера через git в package.json
