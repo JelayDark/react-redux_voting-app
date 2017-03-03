ff0a7b36ec227b25d11e2390299da438a827e6f4
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-03-03 15:15:06 +0200

    Started wrking with client interface nothing works :D

--- voteserver/index.js
+++ voteserver/index.js
@@ -4,3 +4,9 @@ import startServer from './scr/server';
 export const store = makeStore();
 startServer(store);
 //Добавляю команду выполнения запуска сервера через git в package.json
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
