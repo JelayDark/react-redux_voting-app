c506941c92ed55486a5330070ed23362fe02103a
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-03-07 03:53:19 +0200

    Done connecting of actions with interface

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai';
 import reducer from '../src/reducer';
 
 describe('reducer', () => {
-  ///////////////////////////////////////HERE BLYAT :(((
+
     it('Handles SET_STATE', () => {
       const initialState = Map();
       const action = {
@@ -66,4 +66,66 @@ describe('reducer', () => {
         }
       }));
     });
+
+    it('treats VOTE with through appointment hasVoted', () => {
+      const state = fromJS({
+        vote: {pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    }));
+  });
+
+  it('in case of incorrect note doesnt appoint hasVoted for VOTE', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Sunshine'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting','28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }));
+  });
+
+  it('if pair has been changed, cleanes hasVoted in SET_STATE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    });
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Sunshine', 'Slumdog Millionaire']
+        }
+      }
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Sunshine', 'Slumdog Millionaire']
+      }
+    }));
+  });
+
 })
